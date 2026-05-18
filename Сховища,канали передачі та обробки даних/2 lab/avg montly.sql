25c502f2fe9fad5ece8918186e2e182473a5c5e5
Kateryna Danylchenko <kdanilchenko265@gmail.com>
2026-05-18 15:03:37 +0300

    Init web project lab1-4, fix git root

--- Сховища,канали передачі та обробки даних/2 lab/avg montly.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-	order_id,
-    DATE_TRUNC('month', order_date) AS month,
-    ROUND(AVG(total_price) OVER (PARTITION BY DATE_TRUNC('month', order_date)), 2) AS avg_check
-FROM orders
-ORDER BY month;
--- Сховища,канали передачі та обробки даних/2 lab/top25.sql
+++
@@ -1,8 +0,0 @@
-SELECT p.product_name, SUM(oi.quantity * oi.price) as total
-FROM order_items as oi
-JOIN products as p ON oi.product_id = p.product_id
-GROUP BY p.product_name
-ORDER BY total DESC
-LIMIT 25
-
-
